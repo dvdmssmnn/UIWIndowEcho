6e9b012f042dd39d404d577cc7cd7d6a98fc6ef3
David Missmann <davidmissmann@gmail.com>
2016-10-08 12:14:49 +0200

    Initial Commit

---
+++ UIWindowEcho/UIWindowEcho.h
@@ -0,0 +1,19 @@
+//
+//  UIWindowEcho.h
+//  UIWindowEcho
+//
+//  Created by David Missmann on 08.10.16.
+//  Copyright © 2016 David Missmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIWindowEcho.
+FOUNDATION_EXPORT double UIWindowEchoVersionNumber;
+
+//! Project version string for UIWindowEcho.
+FOUNDATION_EXPORT const unsigned char UIWindowEchoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIWindowEcho/PublicHeader.h>
+
+
